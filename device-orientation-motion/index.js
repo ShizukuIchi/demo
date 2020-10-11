99a954b92dee2aa1e113c4d43d6080ed6e1a398e
ShizukuIchi <shizukuichi@gmail.com>
2020-10-11 16:41:34 +0800

    request permission

--- device-orientation-motion/index.js
+++ device-orientation-motion/index.js
@@ -1,20 +1,48 @@
 const spans = document.querySelectorAll('span');
 const [alpha, beta, gamma, ax, ay, az] = spans;
+function listenOrientation() {
+  if (DeviceOrientationEvent) {
+    function handleDeviceOrientation(event) {
+      alpha.textContent = event.alpha;
+      beta.textContent = event.beta;
+      gamma.textContent = event.gamma;
+    }
+    if (DeviceOrientationEvent.requestPermission) {
+      DeviceOrientationEvent.requestPermission().then((state) => {
+        if (state === 'granted') {
+          window.addEventListener(
+            'deviceorientation',
+            handleDeviceOrientation,
+            true,
+          );
+        }
+      });
+    } else {
+      window.addEventListener(
+        'deviceorientation',
+        handleDeviceOrientation,
+        true,
+      );
+    }
+  }
+}
 
-window.addEventListener(
-  'deviceorientation',
-  function(event) {
-    alpha.textContent = event.alpha;
-    beta.textContent = event.beta;
-    gamma.textContent = event.gamma;
-  },
-  true,
-);
-
-window.addEventListener('devicemotion', function(event) {
-  console.log('hi', event.acceleration, event.accelerationIncludingGravity);
-  const { x, y, z } = event.acceleration;
-  ax.textContent = `${x} m/s2`;
-  ay.textContent = `${y} m/s2`;
-  az.textContent = `${z} m/s2`;
-});
+function listenMotion() {
+  if (DeviceMotionEvent) {
+    function handleDeviceMotion(event) {
+      const { x, y, z } = event.acceleration;
+      ax.textContent = `${x} m/s2`;
+      ay.textContent = `${y} m/s2`;
+      az.textContent = `${z} m/s2`;
+    }
+    if (DeviceMotionEvent.requestPermission) {
+      DeviceMotionEvent.requestPermission().then((state) => {
+        if (state === 'granted') {
+          window.addEventListener('devicemotion', handleDeviceMotion, true);
+        }
+      });
+    } else {
+      window.addEventListener('devicemotion', handleDeviceMotion, true);
+    }
+  }
+}
